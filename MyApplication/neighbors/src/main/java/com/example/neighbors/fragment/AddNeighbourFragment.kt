f4875c4bd70e34ec61d3e371780a23a22087e0f6
n-tamanini <n.tamanini@net.estia.fr>
2020-10-31 16:29:46 +0100

    add url validator for image and website fields

--- MyApplication/neighbors/src/main/java/com/example/neighbors/fragment/AddNeighbourFragment.kt
+++ MyApplication/neighbors/src/main/java/com/example/neighbors/fragment/AddNeighbourFragment.kt
@@ -8,6 +8,7 @@ import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
 import androidx.fragment.app.Fragment
 import com.example.neighbors.R
 import com.example.neighbors.data.NeighborRepository
@@ -46,7 +47,7 @@ class AddNeighbourFragment: Fragment(), TextWatcher {
         saveButton.setOnClickListener{
 
             // On récupère la taille de la liste des voisins pour incrémenter automatiquement l'ID
-            val numero = NeighborRepository.getInstance().getNeighbours().size + 1
+            val newNeighborId = NeighborRepository.getInstance().getNeighbours().size + 1
 
             val name: String = nameField.text.toString()
             val address:String = emailAddressField.text.toString()
@@ -56,7 +57,7 @@ class AddNeighbourFragment: Fragment(), TextWatcher {
             val image: String = imageField.text.toString()
 
             val newNeighbour = Neighbor(
-                id = numero.toLong(),
+                id = newNeighborId.toLong(),
                 name = name,
                 address = address,
                 phoneNumber = phone,
@@ -95,6 +96,16 @@ class AddNeighbourFragment: Fragment(), TextWatcher {
             phoneField.setError("Format must be 0X XX XX XX XX")
         }
 
+        val imageUrlValid: Boolean = isValidUrl(imageField.text)
+        if(!imageUrlValid && image_field_not_null){
+            imageField.setError("Invalid image URL")
+        }
+
+        val websiteUrlValid: Boolean = isValidUrl(websiteField.text)
+        if(!websiteUrlValid && website_field_not_null){
+            websiteField.setError("Invalid URL")
+        }
+
         saveButton.isEnabled =
             name_field_not_null
                     && emailAddress_field_not_null
@@ -104,6 +115,8 @@ class AddNeighbourFragment: Fragment(), TextWatcher {
                     && image_field_not_null
                     && emailValid
                     && phoneNumberValid
+                    && imageUrlValid
+                    && websiteUrlValid
     }
 
     fun isValidEmail(target: CharSequence?): Boolean {
@@ -114,6 +127,10 @@ class AddNeighbourFragment: Fragment(), TextWatcher {
         return (((target.toString()).startsWith("07")
                 || (target.toString()).startsWith("06"))
                 && target.toString().length == 10)
+    }
+
+    fun isValidUrl(target: CharSequence?): Boolean{
+        return URLUtil.isValidUrl(target.toString())
     }
 
     // TextWatcher
